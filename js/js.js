739ab060f6e75d343480baa86ad790c6ed29dc03
Waripolo1 <39135676+Waripolo1@users.noreply.github.com>
2019-04-25 21:14:06 -0500

    added cool css

--- js/js.js
+++ js/js.js
@@ -2,18 +2,31 @@ var timer = null;
 var countdownNumber = 10;
 
 var changeState = function(state){
-	document.body.className = 'body-state'+ state;
+	document.body.className = 'body-state'+state;
 		clearInterval(timer);
 		countdownNumber = 10;
-			document.getElementById('countdown').innerHTML = countdownNumber;
-					countdownNumber	= countdownNumber-1;			
+			document.getElementById('countdown').innerHTML = countdownNumber;			
 
 	if (state==2) {
 		timer = setInterval(function(){
+			countdownNumber	= countdownNumber-1;				
 			document.getElementById('countdown').innerHTML = countdownNumber;
-					countdownNumber	= countdownNumber-1;
-				if (countdownNumber <= 0) {
-					changeState(3)
+
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				document.getElementById('nervous').className = 'nervous show';
+			} else{
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+				
+			if (countdownNumber <= 0) {
+				changeState(3)
 				};
 		}, 500);
 	} else if (state==3) {
